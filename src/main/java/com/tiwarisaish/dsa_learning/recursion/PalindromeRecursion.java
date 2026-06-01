e5e6d1f6125eaf6cb0cba4a5e02ae257f5616913
SaishTiwari <tiwarisaish381@gmail.com>
2026-05-31 22:48:11 -0400

    recursion

---
+++ src/main/java/com/tiwarisaish/dsa_learning/recursion/PalindromeRecursion.java
@@ -0,0 +1,32 @@
+package com.tiwarisaish.dsa_learning.recursion;
+
+import java.util.Scanner;
+
+public class PalindromeRecursion {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end)
+            return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result)
+            System.out.println("It is a palindrome");
+        else
+            System.out.println("Not a palindrome");
+
+        sc.close();
+    }
+}
